8eecf27babd9d1fe29dcacc32ff317e581be0c66
scottrobel <robelrain@gmail.com>
2020-02-03 08:56:12 -0500

    write tests for User model

--- test/models/invite_test.rb
+++ test/models/invite_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class InviteTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @invite = invites(:declined_invite)
+    @invited_user = @invite.user
+    @invite_event = @invite.event
+    @invite_status = @invite.invite_status
+  end
+
+  test 'associations not nil' do
+    assert_not_nil @invited_user
+    assert_not_nil @invite_event
+    assert_not_nil @invite_status
+  end
+
+  test 'associations from correct class' do
+    assert_kind_of Invite, @invite
+    assert_kind_of User, @invited_user
+    assert_kind_of InviteStatus, @invite_status
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @declined_user = users(:non_coder)
+    @accepted_user = users(:coder)
+    @pending_user = users(:busy_guy)
+  end
+
+  test 'declined_user has 1 declined_events' do
+    assert_equal @declined_user.declined_events.count, 1
+  end
+
+  test 'pending_user has 1 pending_event' do
+    assert_equal @pending_user.pending_events.count, 1
+  end
+
+  test 'accepted_user has 1 accepted_event' do
+    assert_equal @accepted_user.accepted_events.count, 1
+  end
 end
