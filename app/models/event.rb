d0ec6c8f197fc1717f7a82d9d7639658b052ee7f
scottrobel <robelrain@gmail.com>
2020-02-02 19:22:15 -0500

    wrote code for invited_users test

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,5 @@
 class Event < ApplicationRecord
   belongs_to :event_organizer, class_name: 'User', foreign_key: 'event_organizer_id'
+  has_many :invites
+  has_many :invited_users, through: :invites, source: 'user'
 end
--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -3,14 +3,15 @@ require 'test_helper'
 class EventTest < ActiveSupport::TestCase
   def setup
     @event = events(:coding_party)
+    @event_organizer = users(:party_monster)
   end
 
   test "Events event_organizer attribute points to the right user" do
-    event_organizer = users(:party_monster)
-    assert_equal @event.event_organizer, event_organizer
+    assert_equal @event.event_organizer, @event_organizer
   end
 
   test "invited_users attribute" do
-    assert_count @event.invited_users, 3
+    assert_equal @event.invited_users.count, 3
+    assert_equal @event.invited_users.first.class, User.new.class
   end
 end
