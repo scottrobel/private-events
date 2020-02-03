ce48d4f06d1af7658cd6f0cbda33aac0abbfd7a3
scottrobel <robelrain@gmail.com>
2020-02-03 12:08:27 -0500

    create login page

--- test/controllers/sessions_controller_test.rb
+++ test/controllers/sessions_controller_test.rb
@@ -1,8 +1,16 @@
 require 'test_helper'
 
 class SessionsControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:party_monster)
+    @user.password = 'password'
+  end
+
   test 'login page' do
     get login_path
-    assert_template 'sessions/new'
+    assert_select 'input[name=?]', 'session[username]'
+    assert_select 'input[name=?]', 'session[password]'
+    assert_select 'input[name=?]', 'commit'
   end
 end
