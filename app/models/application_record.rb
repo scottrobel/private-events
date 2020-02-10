6599faf5417e3d9ea770f85531a0d6cb21716342
scottrobel <robelrain@gmail.com>
2020-02-10 16:33:02 -0500

    fix model

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  AcceptedInviteStatusId = InviteStatus.find_or_create_by(status: :accepted)
+  PendingInviteStatusId = InviteStatus.find_or_create_by(status: :pending)
+  DeclinedInviteStatusId = InviteStatus.find_or_create_by(status: :declined)
 end
